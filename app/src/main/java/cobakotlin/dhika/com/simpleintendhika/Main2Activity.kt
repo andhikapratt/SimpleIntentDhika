0a3d947bfdb2a165a09617e4439c0a6b738d720b
Andhika Pratama <advanvandroid5566@gmail.com>
2019-10-18 14:03:48 +0700

    intent graphic

---
+++ app/src/main/java/cobakotlin/dhika/com/simpleintendhika/Main2Activity.kt
@@ -0,0 +1,40 @@
+package cobakotlin.dhika.com.simpleintendhika
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        val bundle:Bundle? = intent.extras
+        val nim_bro = bundle?.get("nim")
+        val nama_bro = bundle?.get("nama")
+        val nil_bro = bundle?.get("nil")
+
+        var nil = nil_bro.toString()
+        var nill = nil.toInt()
+
+        if (nill >= 80){
+            tv_has_ket.setText("A")
+        }
+        else if(nill >= 60){
+            tv_has_ket.setText("B")
+        }
+        else if(nill >= 40){
+            tv_has_ket.setText("C")
+        }
+        else if(nill >= 20){
+            tv_has_ket.setText("D")
+        }
+        else{
+            tv_has_ket.setText("E")
+        }
+        tv_has_nim.setText(""+nim_bro)
+        tv_has_nama.setText(""+nama_bro)
+        tv_has_nil.setText(""+nil_bro)
+    }
+}
---
+++ app/src/main/java/cobakotlin/dhika/com/simpleintendhika/MainActivity.kt
@@ -0,0 +1,27 @@
+package cobakotlin.dhika.com.simpleintendhika
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var nimnya: EditText = findViewById(R.id.et_nim)
+        var namanya: EditText = findViewById(R.id.et_nama)
+        var nilainya: EditText = findViewById(R.id.et_nilai)
+
+        bt_click.setOnClickListener(){
+            intent = Intent(this, Main2Activity::class.java)
+            intent.putExtra("nim",nimnya.getText())
+            intent.putExtra("nama",namanya.getText())
+            intent.putExtra("nil",nilainya.getText())
+            startActivity(intent)
+        }
+    }
+}
